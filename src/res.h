7a067a0687471de49e1233e06aacdff8d3bbd374
Liu Duoyuan <duoyuan.liu@mommydaddyme.com>
2019-02-01 09:29:51 +0800

    first commit

---
+++ src/res.h
@@ -0,0 +1,13 @@
+// width = 22, hegiht = 50
+extern const color_t data_char0_bmp[];
+extern const color_t data_char1_bmp[];
+extern const color_t data_char2_bmp[];
+extern const color_t data_char3_bmp[];
+extern const color_t data_char4_bmp[];
+extern const color_t data_char5_bmp[];
+
+// width = 32, hegiht = 16
+const color_t data_tile000_bmp[];
+const color_t data_tile001_bmp[];
+const color_t data_tile002_bmp[];
+const color_t data_tile003_bmp[];
